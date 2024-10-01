95a6f1ee730103e01d2786106f7179a1a860d5cd
Clint Eastman <clintdev01@gmail.com>
2024-10-01 14:39:16 -0400

    updated cors setting

--- main.go
+++ main.go
@@ -40,11 +40,7 @@ func main() {
 	app := fiber.New()
 	app.Use(logger.New())
 
-	app.Use(cors.New(cors.Config{
-		AllowOrigins: "http://localhost:3000, http://127.0.0.1:3000, https://reviewit.lugetech.com/",
-		AllowMethods: "GET,POST,PUT,DELETE,OPTIONS",
-		AllowHeaders: "Origin, Content-Type, Accept, Authorization",
-	}))
+	app.Use(cors.New())
 
 	if err := initDB(); err != nil {
 		log.Fatalf("Failed to initialize database: %v", err)
